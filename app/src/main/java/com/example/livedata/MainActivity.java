e4daf215649b9612e234636dc04d79f3616fc52d
Aditya Narayan <aditya@avaniinfosoft.com>
2026-08-01 16:48:55 +0530

    - Added View Binding. - Updated UI.

--- app/src/main/java/com/example/livedata/MainActivity.java
+++ app/src/main/java/com/example/livedata/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.livedata;
 
 import android.os.Bundle;
-import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -11,75 +9,75 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.textfield.TextInputEditText;
+import com.example.livedata.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
     MainViewModel model;
-    TextView liveTextView;
-    TextInputEditText editTextView;
-    MaterialButton dataChangeButton;
+    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
 
-        // Find views
-        findViews();
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        applyWindowInsets();
 
         // Get ViewModel
         model = new ViewModelProvider(this).get(MainViewModel.class);
 
         // METHOD 1:
-
         // Create the observer which updates the UI.
+        // This Observer can be reused or removed later.
         final Observer<String> dataObserver = new Observer<String>() {
             @Override
             public void onChanged(String s) {
                 // Update the UI
-                liveTextView.setText(s);
+                binding.liveTextView.setText(s);
             }
         };
         // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
         model.getLiveText().observe(this, dataObserver);
 
+        // Reusing same Observer
+        // anotherLiveData.observe(this, dataObserver);
+
+        // Removing the Observer
+         model.getLiveText().removeObserver(dataObserver);
+
 
         // METHOD 2:
-        // Observe LiveData
+        // 1. Observe LiveData (Without Lambda)
         model.getLiveText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(String s) {
                 // Automatically runs whenever the value changes
-                liveTextView.setText(s);
+                binding.liveTextView.setText(s);
             }
         });
 
+        // OR
+        // 2. Observe LiveData (With Lambda)
+        // model.getLiveText().observe(this, text -> binding.liveTextView.setText(text));
 
 
         // Change the value when button is clicked
-        dataChangeButton.setOnClickListener(v ->{
+        binding.btnChange.setOnClickListener(v -> {
             // Updates LiveData
-            model.setLiveText(String.valueOf(editTextView.getText()));
+             model.setLiveText(String.valueOf(binding.editTextView.getText()));
         });
+    }
 
-
-
-
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+    private void applyWindowInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
 
-    private void findViews() {
-        liveTextView = findViewById(R.id.liveTextView);
-        editTextView = findViewById(R.id.editTextView);
-        dataChangeButton = findViewById(R.id.btnChange);
-    }
 }
